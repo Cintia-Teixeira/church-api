4a059506cbc9b5198d01f3c43cde5af231f1dd33
Cintia <cin.contaadm@gmail.com>
2020-10-20 13:57:49 -0300

    Teste E2E - método upload de imagens.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { config } from 'dotenv';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GalleryModule } from './domains/gallery/gallery.module';
+
+
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    GalleryModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Image],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ test/domains/gallery/gallery.e2e-spec.ts
@@ -0,0 +1,55 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import * as request from 'supertest';
+import { config } from 'dotenv';
+
+import { GalleryModule } from '../../../src/domains/gallery/gallery.module';
+
+config();
+
+describe('Gallery', () => {
+    let app: INestApplication;
+    let img = {
+        id: 1,
+        path: 'test/uploads/image.png'
+    };
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [
+                GalleryModule,
+                TypeOrmModule.forRoot({
+                    type: 'mysql',
+                    host: process.env.DB_HOST,
+                    port: parseInt(process.env.DB_PORT),
+                    username: process.env.DB_USER,
+                    password: process.env.DB_PASS,
+                    database: process.env.DB_TEST_NAME,
+                    entities: [Image],
+                    synchronize: true
+                })
+            ]
+        })
+            .compile();
+
+        app = moduleRef.createNestApplication();
+        app.init();
+    });
+
+    describe('/POST gallery', () => {
+        it('should upload a photo', async () => {
+            return await request(app.getHttpServer())
+                .post('/gallery/upload')
+                .send(img)
+                .expect(201);
+        });
+        console.log('test');
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+})
