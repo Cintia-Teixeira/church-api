ba3c52827357511057cb00ef665e9f4e51d7eb8e
Cintia <cin.contaadm@gmail.com>
2021-02-12 09:55:37 -0300

    Inclusão do header Location no upload de imagens

--- src/domains/gallery/gallery.controller.ts
+++ src/domains/gallery/gallery.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Get, Delete, Param } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Get, Delete, Param, HttpStatus } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { GalleryService } from './gallery.service';
 import { Image } from 'src/common/models/image.entity';
+import { NestResponse } from './../../core/http/nest-response';
+import { NestResponseBuilder } from '../../core/http/nest-response-builder';
 
 
-@Controller('gallery')
+@Controller('galeria')
 export class GalleryController {
     constructor(private galleryService: GalleryService) { }
 
@@ -14,11 +16,18 @@ export class GalleryController {
         return this.galleryService.listImages();
     }
 
-    @Post('upload')
+    @Post()
     @UseInterceptors(FileInterceptor('img'))
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    uploadImage(@UploadedFile() img): Promise<Image> {
-        return this.galleryService.uploadImage(img);
+    async uploadImage(@UploadedFile() img): Promise<NestResponse> {
+        const uploaded = await this.galleryService.uploadImage(img);
+        return new NestResponseBuilder()
+            .setStatus(HttpStatus.CREATED)
+            .setHeaders({
+                'Location': `/galeria/${uploaded.path}`
+            })
+            .setBody(uploaded)
+            .build();
     }
 
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
